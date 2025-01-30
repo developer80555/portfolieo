b7d41c0bce2f9252efc0367ab52de5ad88296389
govindsdr <kusumsdr8055@gmail.com>
2025-01-30 17:17:31 +0530

    It's Me

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,147 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+
+const Navbar = ({ isDarkMode, setIsDarkMode }) => {
+  const [isScrool, setIsScrool] = useState(false);
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScrool(true);
+      } else {
+        setIsScrool(false);
+      }
+    });
+  }, []);
+  return (
+    <>
+      <div className="fixed top-0 right-0 w-11/12 translate-y-[-60%] -z-10 dark:hidden">
+        <Image src={assets.header_bg_color} alt="banner" className="w-full" />
+      </div>
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScrool
+            ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20"
+            : ""
+        }`}
+      >
+        <a href="#top">
+          <Image
+            src={isDarkMode ? assets.logo_dark : assets.logo}
+            alt="logo"
+            className="w-40 cursor-pointer mr-14"
+          />
+        </a>
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 font-semibold ${
+            isScrool
+              ? ""
+              : "bg-white shadow-md bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"
+          } `}
+        >
+          <li>
+            <a href="#top" className="font-Ovo">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo">
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" className="font-Ovo">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo">
+              Contact me
+            </a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <button onClick={() => setIsDarkMode((prev) => !prev)}>
+            <Image
+              src={isDarkMode ? assets.sun_icon : assets.moon_icon}
+              alt=""
+              className="w-6"
+            />
+          </button>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5  border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50 "
+          >
+            Contact{" "}
+            <Image
+              src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon}
+              alt="contact"
+              className="w-3 font-Ovo"
+            />{" "}
+          </a>
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image
+              src={isDarkMode ? assets.menu_white : assets.menu_black}
+              alt=""
+              className="w-6"
+            />
+          </button>
+        </div>
+
+        {/* Mobile Menu  */}
+        <ul
+          ref={sideMenuRef}
+          className="font-semibold flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white"
+        >
+          <div className=" absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={isDarkMode ? assets.close_white : assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a href="#top" className="font-Ovo" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" className="font-Ovo" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
